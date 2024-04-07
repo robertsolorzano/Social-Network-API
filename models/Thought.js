01aa6d3ab8fa52a8bb565d036e7b9ee59b932a92
Robert Solorzano <robertsolorzano510@gmail.com>
2024-04-07 12:39:58 -0700

    Created Thought Model
    - Defined the Thought model using moggose
    - Added fields for thoughtText, createAt, username, and reactions
    - Implemented thoughtText validation with mon and max length
    - Set default value for createAt and format timestamp using a getter
    - Embed the reactSchema as an array in the reactions field
    - Included getters and virtual fields in the JSON output

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: function (timestamp) {
+      return new Date(timestamp).toLocaleString();
+    },
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [reactionSchema],
+}, {
+  toJSON: {
+    getters: true,
+    virtuals: true,
+  },
+  id: false,
+});
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
