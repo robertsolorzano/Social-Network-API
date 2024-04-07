c2e476ebf2fb9e9ce4caaa41849aa90f13f053f3
Robert Solorzano <robertsolorzano510@gmail.com>
2024-04-07 12:47:09 -0700

    Create Reaction schema
    - Defined the Reaction schema using Mongoose
    - Added fields for reactionId, reactionBody, username, and createdAt
    - Set the default value for reactionId using new ObjectId
    - Implemented reactionBody validtaion with required and maxlength
    - Set default value for createdAt and format timestamp using a getter
    - Included getters in the JSON output
    - Disabled the default _id field

---
+++ models/Reaction.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const reactionSchema = new mongoose.Schema({
+  reactionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: function (timestamp) {
+      return new Date(timestamp).toLocaleString();
+    },
+  },
+}, {
+  toJSON: {
+    getters: true,
+  },
+  _id: false,
+});
+
+module.exports = reactionSchema;
