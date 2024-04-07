476bb665d7d00b193201e3370857ff73d66a7bf7
Robert Solorzano <robertsolorzano510@gmail.com>
2024-04-07 14:42:43 -0700

    Implement userController
    - Addd functions for user CRUD operations
    - Integrate with User and Thought models for database interactions

---
+++ controllers/userController.js
@@ -0,0 +1,104 @@
+const { User, Thought } = require('../models');
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().populate('thoughts').populate('friends');
+    res.json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.userId })
+      .populate('thoughts')
+      .populate('friends');
+    if (!user) {
+      return res.status(404).json({ message: 'No user with this id!' });
+    }
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    );
+    if (!user) {
+      return res.status(404).json({ message: 'No user with this id!' });
+    }
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findOneAndDelete({ _id: req.params.userId });
+    if (!user) {
+      return res.status(404).json({ message: 'No user with this id!' });
+    }
+    await Thought.deleteMany({ username: user.username });
+    res.json({ message: 'User and associated thoughts deleted!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const addFriend = async (req, res) => {
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendId } },
+      { new: true }
+    ).populate('friends');
+    if (!user) {
+      return res.status(404).json({ message: 'No user with this id!' });
+    }
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const removeFriend = async (req, res) => {
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } },
+      { new: true }
+    ).populate('friends');
+    if (!user) {
+      return res.status(404).json({ message: 'No user with this id!' });
+    }
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+};
