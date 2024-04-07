00e3229150077086975e9904d1d6f48aabc3649d
Robert Solorzano <robertsolorzano510@gmail.com>
2024-04-07 15:35:17 -0700

    Removed depricated options
    Removed useNewUrlParser and useUnifiedTopology as they have no effect
    anymore
    
    '(node:24277) [MONGODB DRIVER] Warning: useNewUrlParser is a deprecated option: useNewUrlParser has no effect since Node.js Driver version 4.0.0 and will be removed in the next major version
    
    (Use `node --trace-warnings ...` to show where the warning was
    created)
    
    (node:24277) [MONGODB DRIVER] Warning: useUnifiedTopology is a deprecated option: useUnifiedTopology has no effect since Node.js Driver version 4.0.0 and will be removed in the next major version'

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,7 @@ require('dotenv').config();
 
 const connectionString = process.env.MONGODB_URI;
 
-mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(connectionString)
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Error connecting to MongoDB:', error));
 
